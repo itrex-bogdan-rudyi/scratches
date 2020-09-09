26038444ae77be5cf07886126f768c46407f52c4
bogdan.rudyi <bogdan.rudyi@itrexgroup.com>
2020-09-10 01:18:24 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import { performance } from "perf_hooks";
+
+const readFileDelay = 1000
+const convertFileDelay = 100
+const addToDocumentDelay = 10
+const urls = ['google.com', 'yahoo.com', 'aol.com', 'netscape.com', 'nt.com', 'bbc.com', 'fcbarcelona.com', 'netflix.com', 'fb.com', 'mozilla.com'];
+
+function runSequentialFlow() {
+    const start = performance.now();
+    const document: { pages: number}[] = [];
+
+    for (let i = 0; i < urls.length; i++) {
+        const rawFile = readFileSync(urls[i], i + 1)
+        const pdfFile = convertFileSync(rawFile)
+        document.push(addToDocumentSync(pdfFile));
+    }
+    console.log('\n>> total time [sequential]: '.concat(String(performance.now() - start)).concat(' ms\n'));
+}
+
+async function runParallelFlow() {
+    const start = performance.now();
+    const document: { pages: number}[] = [];
+
+    // Read files
+    const rawFilesPromises = urls.map((url, index) => readFileAsync(url, index + 1));
+    const rawFiles = await Promise.all(rawFilesPromises);
+
+    // Convert files and add to document
+    for (let i = 0; i < rawFiles.length; i++) {
+        const pdfFile = convertFileSync(rawFiles[i])
+        document.push(addToDocumentSync(pdfFile));
+    }
+    console.log('\n>> total time [parallel]: '.concat(String(performance.now() - start)).concat(' ms\n'));
+}
+
+function readFileSync(url: string, id: number): { name: string, id: number } {
+    sleep(readFileDelay); 
+    return {name: url, id };
+}
+
+function convertFileSync(rawFile: { name: string; id: number }): { pages: number, body: Buffer, id: number } {
+    sleep(convertFileDelay); 
+    return {pages: 5, body: new Buffer(rawFile.name), id: rawFile.id};
+}
+
+function addToDocumentSync(pdfFile: { pages: number, body: Buffer, id: number }): { pages: number } {
+    sleep(addToDocumentDelay); 
+    return {pages: pdfFile.pages-2};
+}
+
+
+function readFileAsync(url: string, id: number): Promise<{ name: string, id: number }> {
+    return new Promise((resolve) => {
+        setTimeout(() => { 
+            resolve({ name: url, id });
+        }, readFileDelay);
+    });
+}
+
+function sleep(milliseconds: number) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+runParallelFlow();
+runSequentialFlow();
+
